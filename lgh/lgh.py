f290c660bfe18e4ebd9bb837f0d87d47104c13e7
matteli <matthieu.nue@gmail.com>
2022-12-30 22:14:02 +0100

    classify

--- lgh/lgh.py
+++ lgh/lgh.py
@@ -2,130 +2,134 @@
 
 import re
 
-pieces = (
-    (
-        (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 18, (1, "A", "↱")),
-        (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 33, (2, "A", "↰")),
-        (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 18, (32, "A", "↲")),
-        (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 33, (16, "A", "↳")),
-    ),
-    (
-        (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 2, (1, "B", "→")),
-        (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 32, (2, "B", "↓")),
-        (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 16, (32, "B", "←")),
-        (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 1, (16, "B", "↑")),
-    ),
-    (
-        (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 16, (1, "B", "↓")),
-        (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 1, (2, "B", "←")),
-        (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 2, (32, "B", "↑")),
-        (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 32, (16, "B", "→")),
-    ),
-    (
-        (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 1, (16, "G", "↑")),
-        (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 2, (1, "G", "→")),
-        (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 32, (2, "G", "↓")),
-        (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 16, (32, "G", "←")),
-    ),
-    (
-        (3, (0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14), 2, (1, "R", "→")),
-        (17, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 16, (1, "R", "↓")),
-        (3, (0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14), 1, (2, "R", "←")),
-        (17, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 1, (16, "R", "↑")),
-    ),
-    (
-        (3, (0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14), 0, (0, "", "")),
-        (17, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 0, (0, "", "")),
-    ),
-)
-solution = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
 
+class LGH:
+    def __init__(self, ghost_grid):
+        self.pieces = (
+            (
+                (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 18, (1, "A", "↱")),
+                (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 33, (2, "A", "↰")),
+                (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 18, (32, "A", "↲")),
+                (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 33, (16, "A", "↳")),
+            ),
+            (
+                (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 2, (1, "B", "→")),
+                (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 32, (2, "B", "↓")),
+                (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 16, (32, "B", "←")),
+                (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 1, (16, "B", "↑")),
+            ),
+            (
+                (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 16, (1, "B", "↓")),
+                (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 1, (2, "B", "←")),
+                (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 2, (32, "B", "↑")),
+                (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 32, (16, "B", "→")),
+            ),
+            (
+                (19, (0, 1, 2, 4, 5, 6, 8, 9, 10), 1, (16, "G", "↑")),
+                (35, (0, 1, 2, 4, 5, 6, 8, 9, 10), 2, (1, "G", "→")),
+                (50, (0, 1, 2, 4, 5, 6, 8, 9, 10), 32, (2, "G", "↓")),
+                (49, (0, 1, 2, 4, 5, 6, 8, 9, 10), 16, (32, "G", "←")),
+            ),
+            (
+                (3, (0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14), 2, (1, "R", "→")),
+                (17, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 16, (1, "R", "↓")),
+                (3, (0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14), 1, (2, "R", "←")),
+                (17, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 1, (16, "R", "↑")),
+            ),
+            (
+                (3, (0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14), 0, (0, "", "")),
+                (17, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 0, (0, "", "")),
+            ),
+        )
+        self.ghost_grid = ghost_grid
+        self.solution = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+        self.nb_solution = 0
 
-def print_torch(color, position):
-    if color == "R":
-        return f"\33[31m{position}\33[0m"
-    if color == "B":
-        return f"\33[34m{position}\33[0m"
-    if color == "G":
-        return f"\33[32m{position}\33[0m"
-    if color == "A":
-        return f"\33[90m{position}\33[0m"
-    return ""
+    def print_torch(self, color, position):
+        if color == "R":
+            return f"\33[31m{position}\33[0m"
+        if color == "B":
+            return f"\33[34m{position}\33[0m"
+        if color == "G":
+            return f"\33[32m{position}\33[0m"
+        if color == "A":
+            return f"\33[90m{position}\33[0m"
+        return ""
 
+    def print_solution(self, solution, ghost_grid):
+        for row in range(0, 4):
+            line = ""
+            for col in range(0, 4):
+                pos_val = 2 ** (col + 4 * row)
+                if pos_val & self.ghost_grid:
+                    line += "o"
+                else:
+                    aprint = False
+                    for s in range(0, 5):
+                        if self.solution[s][1] & pos_val:
+                            line += self.print_torch(
+                                self.pieces[s][self.solution[s][0]][3][1],
+                                self.pieces[s][self.solution[s][0]][3][2],
+                            )
+                            aprint = True
+                            break
+                        if aprint:
+                            break
+                    if not aprint:
+                        line += "."
+            print(line)
 
-def print_solution(solution):
-    for row in range(0, 4):
-        line = ""
-        for col in range(0, 4):
-            pos_val = 2 ** (col + 4 * row)
-            if pos_val & ghost_grid:
-                line += "o"
+    def test_piece(self, piece, rotation, position, grid, ghost):
+        rot = self.pieces[piece][rotation][0]
+        pos = self.pieces[piece][rotation][1][position]
+        gho = self.pieces[piece][rotation][2]
+        tor = self.pieces[piece][rotation][3][0]
+        val_grid = rot * 2**pos
+        val_ghost = gho * 2**pos
+        val_torch = tor * 2**pos
+        if not (grid & val_grid) and not ((self.ghost_grid ^ val_ghost) & val_grid):
+            self.solution[piece][0] = rotation
+            self.solution[piece][1] = val_torch
+            if piece + 1 < len(self.pieces):
+                self.test_piece(
+                    piece + 1,
+                    0,
+                    0,
+                    grid + val_grid,
+                    ghost + val_ghost,
+                )
+            elif grid + val_grid == 65535 and ghost == self.ghost_grid:
+                self.nb_solution += 1
+                print("\n")
+                print(f"Solution {self.nb_solution} :")
+                self.print_solution(self.solution, self.ghost_grid)
+                return
             else:
-                aprint = False
-                for s in range(0, 5):
-                    if solution[s][1] & pos_val:
-                        line += print_torch(
-                            pieces[s][solution[s][0]][3][1],
-                            pieces[s][solution[s][0]][3][2],
-                        )
-                        aprint = True
-                        break
-                    if aprint:
-                        break
-                if not aprint:
-                    line += "."
-        print(line)
+                print("impossible")
+        if position + 1 < len(self.pieces[piece][rotation][1]):
+            self.test_piece(piece, rotation, position + 1, grid, ghost)
+        elif rotation + 1 < len(self.pieces[piece]):
+            self.test_piece(piece, rotation + 1, 0, grid, ghost)
+        return
 
 
-def test_piece(piece, rotation, position, grid, ghost, ghost_grid, solution):
-    rot = pieces[piece][rotation][0]
-    pos = pieces[piece][rotation][1][position]
-    gho = pieces[piece][rotation][2]
-    tor = pieces[piece][rotation][3][0]
-    val_grid = rot * 2**pos
-    val_ghost = gho * 2**pos
-    val_torch = tor * 2**pos
-    if not (grid & val_grid) and not ((ghost_grid ^ val_ghost) & val_grid):
-        solution[piece][0] = rotation
-        solution[piece][1] = val_torch
-        if piece + 1 < len(pieces):
-            test_piece(
-                piece + 1,
-                0,
-                0,
-                grid + val_grid,
-                ghost + val_ghost,
-                ghost_grid,
-                solution,
-            )
-        elif grid + val_grid == 65535 and ghost == ghost_grid:
-            global nb_solution
-            nb_solution += 1
-            print("\n")
-            print(f"Solution {nb_solution} :")
-            print_solution(solution)
-            return
+def main():
+    ghost_grid = 0
+    p = 0
+    print("Indicate with 0 for no ghost and 1 for ghost:")
+    while True:
+        line = input()
+        if not re.match(r"[0-1]{4}$", line):
+            print("Put only 4 digits of 0 or 1")
         else:
-            print("impossible")
-    if position + 1 < len(pieces[piece][rotation][1]):
-        test_piece(piece, rotation, position + 1, grid, ghost, ghost_grid, solution)
-    elif rotation + 1 < len(pieces[piece]):
-        test_piece(piece, rotation + 1, 0, grid, ghost, ghost_grid, solution)
-    return
+            for l in line:
+                ghost_grid += int(l) * 2**p
+                p += 1
+        if p > 15:
+            break
+    lgh = LGH(ghost_grid)
+    lgh.test_piece(0, 0, 0, 0, 0)
 
 
-ghost_grid = 0
-p = 0
-print("Indicate with 0 for no ghost and 1 for ghost:")
-while True:
-    line = input()
-    if not re.match(r"[0-1]{4}$", line):
-        print("Put only 4 digits of 0 or 1")
-    else:
-        for l in line:
-            ghost_grid += int(l) * 2**p
-            p += 1
-    if p > 15:
-        break
-nb_solution = 0
-test_piece(0, 0, 0, 0, 0, ghost_grid, solution)
+if __name__ == "__main__":
+    main()
